5ead4b8cf8e841a002de03a4a98cbd4fcd35c73e
Kamaron <kam.is.amazing@gmail.com>
2015-04-02 19:02:39 -0600

    Changed to Node.JS server

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kamaron on 4/2/15.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000);
+
+console.log('Listening on port 3000...');
---
+++ public/scripts/Blocks.js
@@ -0,0 +1,18 @@
+﻿var Blocks = new function () {
+
+    var newBlock = function (newR, newC, newTexture, amIFilled) {
+
+        return {
+            row: newR,
+            col: newC,
+            texture: newTexture,
+            filled: amIFilled
+        };
+        
+    }
+
+    return {
+        newBlock: newBlock
+    };
+
+}();
